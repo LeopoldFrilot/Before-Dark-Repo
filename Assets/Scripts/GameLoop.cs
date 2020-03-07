28ffc3afb9b27bdccfac7ff7ac30c20e06485bac
LeopoldFrilot <Lfrilo1@lsu.edu>
2020-03-07 11:17:30 -0600

    Scene Switching was updated to automatically find relavant game objects. More tweaks as well

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLoop : MonoBehaviour
 {
+    // Calling External Objects for later use
+    public GameObject sceneSwitch;
+    public GameObject UITime;
+    public GameObject UIHealth;
+
+    // Game Design Knobs
+    public static float maxStartingTime = 300f;
+    public static float maxStartingHealth = 500f;
+
+    // Public static variables are ones that carry over scene to scene
+    public static float timer = maxStartingTime;
+    public static float health = maxStartingHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneSwitch = GameObject.Find("/SceneSwitcher");
+        UITime = GameObject.Find("/Level UI/Time");
+        UIHealth = GameObject.Find("/Level UI/Health");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckPause();
+        UpdateTime();
+        UpdateHealth();
+    }
+
+    private void CheckPause()
+    {
+        //Pause Condition
+        if (Input.GetButtonDown("Cancel"))
+        {
+            sceneSwitch.GetComponent<SceneSwitch>().LoadSettingsScreen();
+        }
+    }
+    public void UpdateTime()
+    {
+        if(timer >= 0 && sceneSwitch.GetComponent<SceneSwitch>().GetCurrSceneIndex() > 0 && sceneSwitch.GetComponent<SceneSwitch>().GetCurrSceneIndex() < 8)
+        {
+            timer -= Time.deltaTime * 0.85f;
+        }
+        else if(timer == 0)
+        {
+            sceneSwitch.GetComponent<SceneSwitch>().LoadLoseScreen();
+        }
+        UITime.GetComponent<Text>().text = "Time: " + (int)timer;
+    }
+    public void UpdateHealth()
+    {
+        UIHealth.GetComponent<Text>().text = "Health: " + (int)health;
+    }
+    public void HardReset()
+    {
+        timer = maxStartingTime;
+        health = maxStartingHealth;
     }
 }
--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,18 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    // External sources
+    public GameObject gameloopScriptHolder; // Current location script is held
+
     // Variables for scenes
     string settingsScn = "Settings Screen";
     string loseScn = "Lose Screen";
 
+    private void Start()
+    {
+        gameloopScriptHolder = GameObject.Find("Main Camera");
+    }
     // Loads next level based on File->Build Settings scene order
     public void LoadNextLevel()
     {
         int currScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currScene + 1);
     }
+    public int GetCurrSceneIndex()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
     public void LoadIntro()
     {
+        GameLoop gameScript = gameloopScriptHolder.GetComponent<GameLoop>();
+
+        gameScript.HardReset();
         SceneManager.LoadScene(0);
     }
 
