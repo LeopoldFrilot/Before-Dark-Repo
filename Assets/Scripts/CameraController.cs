80fc12ff34072aa7380e20bdc5c93350f0a1c4be
m1keG33 <newemail.apps8@gmail.com>
2020-03-07 17:13:42 -0600

    movement and camera in progress

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // target player1 object
+
+    public Vector3 offset; //vector used to offset camera
+
+    public bool useOffsetValues; //optional, if player wants custom offset values
+
+    public float rotateSpeed; //rotation speed of camera
+
+    public Transform pivot; //pivot camera to move with player movement
+
+    public float maxViewAngle; //max and min view for player rotation
+    public float minViewAngle;
+
+    public bool invertY; //if player wants to invert their camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        { //if offset values are turned off this function executes at run time
+            offset = target.position - transform.position;
+        }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = null;
+        Cursor.lockState = CursorLockMode.Locked; //remove cursor from screen
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        pivot.transform.position = target.transform.position; // pivot moves with player
+        //get the x position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed; //rotating camera on x axis
+        pivot.Rotate(0, horizontal, 0); //apllying rotation to pivot rather than target
+
+        //get y position of the mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed; //rotating camera on y axis
+
+        //code block to give players the option of inverting thier camera
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //setting limit for how far up and down the camera can rotate
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        { //eulerAngles.x instead of .x to avoid taking the x value of the Quaternion
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        { //315 is 45 degrees below x axis
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        // move camera based on the current rotation of the target and the orginal offset
+        float desired_Yangle = pivot.eulerAngles.y; //changing target to pivot rotatiion for greater camera control
+        float desired_Xangle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desired_Xangle, desired_Yangle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        transform.LookAt(target);
+
+        if (transform.position.y < target.position.y)
+        { //prevent camera from going through the ground if less than player1 "target position"
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed; //controls movementspeed from inspector
+    public float jumpForce; //used to jump into air
+    public CharacterController controller;
+
+    private Vector3 moveDirection; //vector object ref used to control move direction (x,y,z)
+    public float gravityScale;  //float to control gravity from inspector
+
+    public Animator animate; //used for animation of charactor model
+    public Transform pivot; //setting charachter to turn with camera rotation
+    public float rotation; //rotation speed of character 
+
+    public GameObject playerModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>(); //initializes public controller variable in inspector
+    }
+
+    // Update is called once per frame
+    void Update() //Remark: using rigidbody for movement is better for racing games (eg. jumping a ramp)
+    {
+        //new move direction with rotation of mouse
+        float yStore = moveDirection.y; //save y direction in a float to correct jump
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore; //applying previous up/down movement back to player
+
+        if (controller.isGrounded)
+        { //used to smooth out jumps and falling off ledges, "if player is grounded"
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            { //jump
+                moveDirection.y = jumpForce; //jump is applied in y axis direction
+            }
+        }
+        //to smooth out frames over different systems x Time.deltaTime 
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //move the player in different direction based on camera look direction
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            //used for gradual rotations for smoother camera rotations
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotation * Time.deltaTime); //slerp one of the 3 movement types in unity (for a smooth rotation w/ camera)    
+        }
+
+        //this code animates character to run motion when directional keys are pressed 
+        animate.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
+
+        //exit game with button press "Escape"
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("quit game");
+            Application.Quit();
+        }
+    }
+}
