bd9285ee5a7b62197b58ead6cb6dacc8fc1000c7
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2020-03-25 12:35:21 -0500

    Attacking the goblin registers a hit message

---
+++ Assets/Scripts/GoblinController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "fx_weapon" || col.gameObject.name == "Bip001 Prop2")
+        {
+            Debug.Log("Hit");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
