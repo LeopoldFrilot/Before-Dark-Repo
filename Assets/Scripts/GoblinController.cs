e552f04d74e963686716a0667dbb1444b39ad993
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2020-04-02 14:18:37 -0500

    Goblin is now a navmeshagent that follows the player and vibrates on them once touching.

--- Assets/Scripts/GoblinController.cs
+++ Assets/Scripts/GoblinController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GoblinController : MonoBehaviour
 {
@@ -9,12 +10,13 @@ public class GoblinController : MonoBehaviour
 
     public float lookRadius = 10f;
 
-    Transform targer;
+    Transform target;
     NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -32,7 +34,17 @@ public class GoblinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if(distance <= agent.stoppingDistance)
+            {
+                //Attack player
+            }
+        }
     }
 
     void OnDrawGizmosSelected()
