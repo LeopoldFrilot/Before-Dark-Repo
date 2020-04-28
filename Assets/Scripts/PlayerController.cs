384ce898dc3da5356cf544bb81d5a028a31152e4
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2020-04-28 16:52:30 -0500

    fixed the goblin, the player manager script disappeared so I re-added it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -71,10 +71,10 @@ public class PlayerController : MonoBehaviour
             { //jump
                 moveDirection.y = jumpForce; //jump is applied in y axis direction
             }
-            //else if (Input.GetButtonDown("Fire1"))
-            //{
-            //    animate2.SetTrigger("Player_Attack");
-            //}
+            else if (Input.GetButtonDown("Fire1"))
+            {
+               animate2.SetTrigger("Player_Attack");
+            }
         }
         //to smooth out frames over different systems x Time.deltaTime 
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
