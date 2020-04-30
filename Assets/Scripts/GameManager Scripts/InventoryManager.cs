af3a8b1020e6e6ed128e868c4e2fe9c1e7d812af
LeopoldFrilot <lfrilo1@lsu.edu>
2020-04-30 15:46:49 -0500

    Added Shadowboss and initialized chest spawner

--- Assets/Scripts/GameManager Scripts/InventoryManager.cs
+++ Assets/Scripts/GameManager Scripts/InventoryManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public int initialChestNum = 10;
+
+    // boundaries of the map
+    public float maxPosX = 100f;
+    public float maxNegX = -500f;
+    public float maxPosZ = 100f;
+    public float maxNegZ = -200f;
+    public float dropHeight = 500f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnChests(initialChestNum);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnChests(int num)
+    {
+
     }
 }
