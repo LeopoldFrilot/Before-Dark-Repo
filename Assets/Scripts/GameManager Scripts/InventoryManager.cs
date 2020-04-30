5561f1b29e86163e8425e8a725b7e0b8705be07f
LeopoldFrilot <lfrilo1@lsu.edu>
2020-04-30 16:17:53 -0500

    Added Chest Spawner

--- Assets/Scripts/GameManager Scripts/InventoryManager.cs
+++ Assets/Scripts/GameManager Scripts/InventoryManager.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    public int initialChestNum = 10;
+    public int initialChestNum = 20;
 
     // boundaries of the map
     public float maxPosX = 100f;
@@ -13,7 +14,7 @@ public class InventoryManager : MonoBehaviour
     public float maxNegZ = -200f;
     public float dropHeight = 500f;
 
-
+    public GameObject Chest;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,10 @@ public class InventoryManager : MonoBehaviour
 
     public void SpawnChests(int num)
     {
+        for(int i = 0; i < num; i++)
+        {
+            Instantiate(Chest, new Vector3(UnityEngine.Random.Range(maxNegX, maxPosX), dropHeight, UnityEngine.Random.Range(maxNegZ, maxPosZ)), Quaternion.identity);
+        }
 
     }
 }
