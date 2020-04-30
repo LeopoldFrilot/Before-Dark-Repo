e19773a55ad3a8536b9793e68851f5341e6aab9c
Courtland Crouchet <CourtlandCthulhu@gmail.com>
2020-04-30 16:03:21 -0500

    Added a BossController script identical to the GoblinController and attached it to the Shadow Boss

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    int maxHealth = 100;
+    int curHealth = 100;
+
+    public float lookRadius = 10f;
+    public Animator anim;
+
+    Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Stick.R" || col.gameObject.name == "Bip001 Prop2")
+        {
+            Debug.Log("Hit");
+            curHealth -= 10;
+            if(curHealth <= 0) Destroy(gameObject);
+            Debug.Log(curHealth);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal")))); //tesing animation movement for npc
+            
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attack player
+            }
+        }
+      
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+    
+}
