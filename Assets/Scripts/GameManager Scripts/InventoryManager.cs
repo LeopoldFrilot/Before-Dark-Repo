44981140ee7109cedcc57fa6bce36f4ece52574e
LeopoldFrilot <lfrilo1@lsu.edu>
2020-05-01 10:08:36 -0500

    added groceries to inventory

--- Assets/Scripts/GameManager Scripts/InventoryManager.cs
+++ Assets/Scripts/GameManager Scripts/InventoryManager.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
     public int initialChestNum = 20;
     public int groceriesCollected = 0;
-    public int groceriesToCollect = 5;
+    public int groceriesToCollect = 10;
 
     // boundaries of the map
     public float maxPosX = 100f;
@@ -17,17 +18,24 @@ public class InventoryManager : MonoBehaviour
     public float dropHeight = 50f;
 
     public GameObject Chest;
+    public GameObject slot1;
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnChests(initialChestNum);
+        slot1 = GameObject.Find("Slot (1)");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform number = slot1.transform.Find("Number");
+        if (groceriesCollected <= groceriesToCollect)
+        {
+            number.GetComponent<TextMeshProUGUI>().text = "" + groceriesCollected;
+        }
+
     }
 
     public void SpawnChests(int num)
